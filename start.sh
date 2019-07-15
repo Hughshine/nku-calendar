abb49ffc8eda1d6c43e485d1dd8e98382d6c361e
shrine <1015545250@qq.com>
2019-07-15 16:07:58 +0800

    feat(auto-deploy script): 简单的，不安全的一键部署脚本
    需要安装apache\php\mysql，检查了后两者

---
+++ start.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# Unsafe auto-deploy script.
+user=root
+password="default"
+if [ -n "$1" ]; then
+    user=$1
+    echo Using mysql user: ${user}
+else
+    echo Using default mysql user: ${user}
+fi
+
+if [ -n "$2" ]; then
+    password=$2
+else
+    echo Using empty password for user ${user}
+fi
+
+echo Initiating database: TestShell
+cat ./db/sh_test.sql| mysql -u ${user} -p"${password}"
+
+echo PHP INIT
+php init
+
+echo PROJECT CONFIGING
+
+(
+cat << EOF
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=TestShell',
+            'username' => '${user}',
+            'password' => '${password}',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
+EOF
+) > test.txt
+
+
+echo COMPOSER UPDATE
+composer install
+composer update
