e4d734a4f9ba14a01c1a0c6d014ab120eeedcaf9
shrine <1015545250@qq.com>
2019-07-24 14:00:04 +0800

    fix(更新小作业):

--- frontend/views/homework/person.php
+++ frontend/views/homework/person.php
@@ -4,6 +4,6 @@
 <br>
 <br>
 <a href="./data/personal/李瑞峰/web作业-李瑞峰-1711347.rar" download="李瑞峰1711347.rar" ><span style="font-size:24px;">李瑞峰</span><br /></a>
-<a href="./data/personal/李瑞峰/web作业-李瑞峰-1711347.zip" ><span style="font-size:24px;">李煦阳</span><br /></a>
+<a href="./data/personal/李煦阳/web作业-李煦阳-1711352.zip" download="个人作业(1711352_李煦阳).zip"><span style="font-size:24px;">李煦阳</span><br /></a>
 <a href="https://github.com/Hughshine/nku-calendar/tree/master/data/personal/%E6%9D%8E%E6%B3%93%E8%90%B1" ><span style="font-size:24px;">李泓萱</span><br /></a>
 <a href="https://github.com/Hughshine/nku-calendar/tree/master/data/personal/%E7%8E%8B%E6%96%B0%E5%85%83" ><span style="font-size:24px;">王新元</span><br /></a>
