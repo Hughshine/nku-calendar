87f9ca281680e8c5a875c9a3cc2dde8c6f683787
LiRuiFeng <1326258344@qq.com>
2019-07-24 10:36:41 +0800

    提交了个人作业

--- frontend/views/homework/person.php
+++ frontend/views/homework/person.php
@@ -3,7 +3,7 @@
 </h1>
 <br>
 <br>
-<a href="https://github.com/Hughshine/nku-calendar/tree/master/web%E5%B0%8F%E4%BD%9C%E4%B8%9A/%E6%9D%8E%E7%91%9E%E5%B3%B0" ><span style="font-size:24px;">李瑞峰</span><br /></a>
-<a href="https://github.com/Hughshine/nku-calendar/tree/master/web%E5%B0%8F%E4%BD%9C%E4%B8%9A/%E6%9D%8E%E7%85%A6%E9%98%B3" ><span style="font-size:24px;">李煦阳</span><br /></a>
-<a href="https://github.com/Hughshine/nku-calendar/tree/master/web%E5%B0%8F%E4%BD%9C%E4%B8%9A/%E6%9D%8E%E6%B3%93%E8%90%B1" ><span style="font-size:24px;">李泓萱</span><br /></a>
-<a href="https://github.com/Hughshine/nku-calendar/tree/master/web%E5%B0%8F%E4%BD%9C%E4%B8%9A/%E7%8E%8B%E6%96%B0%E5%85%83" ><span style="font-size:24px;">王新元</span><br /></a>
+<a href="https://github.com/Hughshine/nku-calendar/tree/master/data/personal/%E6%9D%8E%E7%91%9E%E5%B3%B0" ><span style="font-size:24px;">李瑞峰</span><br /></a>
+<a href="https://github.com/Hughshine/nku-calendar/tree/master/data/personal/%E6%9D%8E%E7%85%A6%E9%98%B3" ><span style="font-size:24px;">李煦阳</span><br /></a>
+<a href="https://github.com/Hughshine/nku-calendar/tree/master/data/personal/%E6%9D%8E%E6%B3%93%E8%90%B1" ><span style="font-size:24px;">李泓萱</span><br /></a>
+<a href="https://github.com/Hughshine/nku-calendar/tree/master/data/personal/%E7%8E%8B%E6%96%B0%E5%85%83" ><span style="font-size:24px;">王新元</span><br /></a>
