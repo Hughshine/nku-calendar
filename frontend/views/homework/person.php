d364f58d4560a8cf7d669ff00bb0fe87cbacdc96
LiRuiFeng <1326258344@qq.com>
2019-07-25 15:07:16 +0800

    更新团队作业

--- frontend/views/homework/person.php
+++ frontend/views/homework/person.php
@@ -4,6 +4,6 @@
 <br>
 <br>
 <a href="./data/personal/李瑞峰/web作业-李瑞峰-1711347.rar" download="web作业-李瑞峰-1711347" ><span style="font-size:24px;">李瑞峰</span><br /></a>
-<a href="./data/personal/李煦阳/web作业-李煦阳-1711352.rar" download="web作业-李煦阳-1711352.rar"  ><span style="font-size:24px;">李煦阳</span><br /></a>
-<a href="./data/personal/李泓萱/web作业-李泓萱-1711432.rar" download="web作业-李泓萱-1711432.rar" ><span style="font-size:24px;">李泓萱</span><br /></a>
-<a href="./data/personal/王新元/个人作业(1711384_王新元).rar" download="个人作业(1711384_王新元).rar" ><span style="font-size:24px;">王新元</span><br /></a>
+<a href="./data/personal/李煦阳/web作业-李煦阳-1711352.zip" download="web作业-李煦阳-1711352"  ><span style="font-size:24px;">李煦阳</span><br /></a>
+<a href="./data/personal/李泓萱/web作业-李泓萱-1711432.rar" download="web作业-李泓萱-1711432" ><span style="font-size:24px;">李泓萱</span><br /></a>
+<a href="./data/personal/王新元/个人作业(1711384_王新元).rar" download="个人作业(1711384_王新元)" ><span style="font-size:24px;">王新元</span><br /></a>
